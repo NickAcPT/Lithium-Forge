b705ef5803322e4a02124bbc1264e5a3ef1abd66
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2017-10-04 19:30:17 +0100

    Change license to MIT

--- src/main/java/net/nickac/lithium/frontend/mod/ui/LithiumGUI.java
+++ src/main/java/net/nickac/lithium/frontend/mod/ui/LithiumGUI.java
@@ -1,20 +1,3 @@
-/*
- * This file is part of Lithium.
- *
- * Lithium is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Lithium is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Lithium.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package net.nickac.lithium.frontend.mod.ui;
 
 import net.minecraft.client.Minecraft;
@@ -77,9 +60,9 @@ public class LithiumGUI extends GuiScreen {
 	 */
 	private int centerLoc(LControl c, int s, int w, int x, boolean centered) {
 
-		int parentSizeW = c.getParent() == null ? 0 : c.getParent().getClass().equals(LPanel.class) ? ((LPanel) c.getParent()).getTotalWidth() : ((LControl) c.getParent()).getSize().getWidth();
+		/*int parentSizeW = c.getParent() == null ? 0 : c.getParent().getClass().equals(LPanel.class) ? ((LPanel) c.getParent()).getTotalWidth() : ((LControl) c.getParent()).getSize().getWidth();
 		int parentSizeH = c.getParent() == null ? 0 : c.getParent().getClass().equals(LPanel.class) ? ((LPanel) c.getParent()).getTotalHeight() : ((LControl) c.getParent()).getSize().getHeight();
-
+*/
 		if (centered)
 			return (s / 2) - (w / 2)/* + (parentSizeW != 0 ? parentSizeW / 2 - w /2 : 0)*/;
 
