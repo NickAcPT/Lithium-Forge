6c370daca05dd4b697a4ec73e42d4c72ffbfb04f
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2017-10-23 11:10:20 +0100

    Add missing file

---
+++ src/main/java/net/nickac/lithium/frontend/mod/ui/renders/SliderRenderer.java
@@ -0,0 +1,52 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 NickAc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package net.nickac.lithium.frontend.mod.ui.renders;
+
+import net.minecraft.client.gui.GuiScreen;
+import net.nickac.lithium.backend.controls.impl.LSlider;
+import net.nickac.lithium.backend.other.rendering.ILithiumControlRenderer;
+
+/**
+ * Created by NickAc for Lithium!
+ */
+public class SliderRenderer implements ILithiumControlRenderer<LSlider, GuiScreen> {
+
+	@Override
+	public void renderLithiumControl(LSlider control, GuiScreen gui) {
+
+	}
+
+	@Override
+	public void mouseClick(LSlider control, GuiScreen gui, int mouseX, int mouseY, int mouseButton) {
+
+	}
+
+	@Override
+	public void mouseClickMove(LSlider control, GuiScreen gui, int mouseX, int mouseY, int clickedMouseButton, long timeSinceLastClick) {
+
+	}
+
+}
